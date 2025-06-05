fa1d82beb909a2fe996087fca445330cab1f3b4a
Thomas256-hub <makumbithomas13@gmail.com>
2025-06-05 06:53:22 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My PHP Site</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Hello from PHP!</h1>
+  <?php
+    echo "Today is " . date("l");
+  ?>
+  <script src="script.js"></script>
+</body>
+</html>
